4bcc37415dc072e26ea0ffd44897282cc969e994
JorgePaes1 <129962796+JorgePaes1@users.noreply.github.com>
2023-09-12 16:16:01 -0300

    Update Dicionario.py

--- Python/Dicionario.py
+++ Python/Dicionario.py
@@ -1,1 +1,29 @@
+import funcoes as f
 
+cadastro = [
+    {
+        'nome' : 'Renato',
+        'e-mail': 'renato@email.com',
+        'saldo': 108
+    },
+    {   
+        'nome' : 'Jose',
+        'e-mail': 'Jose@email.com',
+        'saldo': 1024
+
+    },
+    {
+        'nome' : 'Maria',
+        'e-mail': 'Maria@gmail.com',
+        'saldo': 56
+    },
+    {
+        'nome' : 'Joao',
+        'e-mail': 'Joao@gmail.com',
+        'saldo': 50
+    }
+]
+
+extrair_nomes = f.filtrar_nome(cadastro)
+nomes = f.formata_nomes(extrair_nomes, 'CAIXA BAIXA')
+print(f'nomes: {nomes}')
