bd38568cdfb653b70b5703b342b4d10d3c674145
JorgePaes1 <129962796+JorgePaes1@users.noreply.github.com>
2023-10-26 16:16:17 -0300

    Create exercicio.py

---
+++ Python/exercicios/Interface-usuario/exercicio.py
@@ -0,0 +1,6 @@
+'''
+Fazer uma interface com uma lista de dicionarios usando controle de fluxo de tratamento de excenção
+1°- Fazer uma função que receba valores de um cadastro e adicione numa de lista de dicionario.
+2°- Fazer uma função que listar todos os cadastros
+3°- Aplicar o controle de fluxo de tratamento de error
+'''
