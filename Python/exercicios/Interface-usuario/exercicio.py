edf243c1a906533bff5ef2415c0cb5fb23e6bc3e
JorgePaes1 <129962796+JorgePaes1@users.noreply.github.com>
2023-10-31 00:21:44 -0300

    Update exercicio.py

--- Python/exercicios/Interface-usuario/exercicio.py
+++ Python/exercicios/Interface-usuario/exercicio.py
@@ -4,3 +4,40 @@ Fazer uma interface com uma lista de dicionarios usando controle de fluxo de tra
 2°- Fazer uma função que listar todos os cadastros
 3°- Aplicar o controle de fluxo de tratamento de error
 '''
+
+def cadastro():
+    cadastro = []
+    while True:
+        try:
+            nome = input("Digite seu nome:\n")
+            if not all(c.isalpha() or c.isspace() for c in nome):
+                raise ValueError("O nome deve conter apenas letras")
+            idade = input("Digite sua idade:\n")
+            email = input("Digite seu email:\n")
+            cpf = input("Digite seu cpf:\n")
+            dicionario = {"Nome": nome,
+                          "Idade": idade,
+                          "Email": email,
+                          "CPF": cpf,
+                          }
+            cadastro.append(dicionario)
+            resposta = input("Deseja continuar cadastrando? (S/N):\n")
+            if resposta.lower() == "n":
+                break
+        except Exception as e:
+            print(e)
+    return cadastro
+
+
+def listar(cadastro):
+    for pessoa in cadastro:
+        print(f"\nNome: {pessoa['Nome']}")
+        print("Dados:")
+        for chave, valor in pessoa.items():
+            if chave != "Nome":
+                print(f"{chave}: {valor}")
+        print()
+
+
+cadastro = cadastro()
+listar(cadastro)
